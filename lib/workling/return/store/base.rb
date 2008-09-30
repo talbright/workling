a0dea88745356ed1f144321889bf8fb813232f59
Chris Gaffney <cgaffney@crayoninterface.com>
2008-09-30 14:55:22 -0400

    API specifies set not put. Fix the exception thrown.

--- lib/workling/return/store/base.rb
+++ lib/workling/return/store/base.rb
@@ -2,20 +2,20 @@ module Workling
   module Return
     module Store
       mattr_accessor :instance
-      
+
       def self.set(key, value)
         self.instance.set(key, value)
       end
-      
+
       def self.get(key)
         self.instance.get(key)
       end
-      
+
       class Base
         def set(key, value)
-          raise NotImplementedError.new("put(key, value) not implemented in #{ self.class }")
+          raise NotImplementedError.new("set(key, value) not implemented in #{ self.class }")
         end
-        
+
         def get(key)
           raise NotImplementedError.new("get(key) not implemented in #{ self.class }")
         end
