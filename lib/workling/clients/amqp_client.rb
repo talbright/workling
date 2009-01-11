c54916935378d4ee693a885cc3e5d45c60cb74e5
Todd Tyree <toddtyree@hester.(none)>
2009-01-11 16:00:54 +0000

    Added the marshaling and unmarshaling logic to keep rabbit from flattening the passed options out in to a string.

--- lib/workling/clients/amqp_client.rb
+++ lib/workling/clients/amqp_client.rb
@@ -24,13 +24,13 @@ module Workling
       # subscribe to a queue
       def subscribe(key)
         @amq.queue(key).subscribe do |value|
-          yield value
+          yield Marshal.load(value)
         end
       end
       
       # request and retrieve work
       def retrieve(key); @amq.queue(key); end
-      def request(key, value); @amq.queue(key).publish(value); end
+      def request(key, value); @amq.queue(key).publish(Marshal.dump(value)); end
     end
   end
 end
