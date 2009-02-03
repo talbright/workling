0c444e90eb8890964b046d8c6fe355e55860b02a
Justin S. Leitgeb <jsl@hcoop.net>
2009-02-03 15:25:52 -0500

    Remove deprecated call to AR::Base.allow_concurrency

--- lib/workling/remote/invokers/threaded_poller.rb
+++ lib/workling/remote/invokers/threaded_poller.rb
@@ -22,10 +22,7 @@ module Workling
           @mutex = Mutex.new
         end      
           
-        def listen                
-          # Allow concurrency for our tasks
-          ActiveRecord::Base.allow_concurrency = true
-
+        def listen
           # Create a thread for each worker.
           Workling::Discovery.discovered.each do |clazz|
             logger.debug("Discovered listener #{clazz}")
