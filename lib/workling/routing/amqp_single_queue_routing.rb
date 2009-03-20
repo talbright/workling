0068e2b086debe0353be7163a14c3b54d9e8d086
Simon Horne <simon@simon.streetlinenetworks.com>
2009-03-20 13:19:24 -0700

    typo

--- lib/workling/routing/amqp_single_queue_routing.rb
+++ lib/workling/routing/amqp_single_queue_routing.rb
@@ -13,7 +13,7 @@ module Workling
         @method_name = ARGV[1]
         @queue_name = ARGV[2]
 
-        puts "** single queue routing: #{@queue_name}, #{@method_name}, #{@worker_class}"
+        puts "** single queue routing: #{@queue_name}, #{@method_name}, #{@worker.class}"
       end
 
       # TODO - redo this...
