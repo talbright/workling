fcdfb4931813f0a0f24e56cbe43d9748b2330ef8
Simon Horne <simon@simon.streetlinenetworks.com>
2009-03-25 09:42:34 -0700

    backed out the call to flush on the logger - causes a bunch of tests to fail

--- lib/workling/base.rb
+++ lib/workling/base.rb
@@ -44,8 +44,6 @@ module Workling
 
         # reraise after logging. the exception really can't go anywhere in many cases. (spawn traps the exception)
         raise e if Workling.raise_exceptions?
-      ensure
-        logger.flush
       end
     end    
   
