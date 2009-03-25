d54fb1b9996ec7c8d06d28e53aa0901855b06859
Simon Horne <simon@simon.streetlinenetworks.com>
2009-03-25 09:43:29 -0700

    call to Dir.glob now ruby 1.8.5 compatible

--- lib/workling/discovery.rb
+++ lib/workling/discovery.rb
@@ -1,14 +1,17 @@
 #
-#  Discovery is responsible for loading workers in app/workers. 
+#  Discovery is responsible for loading workers in app/workers.
 #
 module Workling
   class Discovery
     cattr_accessor :discovered
     @@discovered = []
-    
-    # requires worklings so that they are added to routing. 
+
+    # requires worklings so that they are added to routing.
     def self.discover!
-      Dir.glob(Workling.load_path.map { |p| "#{ p }/**/*.rb" }).each { |wling| require wling }
+      # ruby 1.8.5 compatible:
+      Workling.load_path.map { |p| Dir.glob("#{ p }/**/*.rb") }.flatten.each { |wling| require wling }
+      # not ruby 1.8.5 compatible:
+      # Dir.glob(Workling.load_path.map { |p| "#{ p }/**/*.rb" }).each { |wling| require wling }
     end
   end
 end
