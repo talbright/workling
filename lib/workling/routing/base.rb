2b7a38c5bfa6dec3b70fa2180714b2a3fbd2f87f
Simon Horne <soulware@gmail.com>
2009-03-27 23:58:10 -0700

    logger now available in routing instance cleanup tests

--- lib/workling/routing/base.rb
+++ lib/workling/routing/base.rb
@@ -5,6 +5,10 @@
 module Workling
   module Routing
     class Base < Hash
+      @@logger ||= ::RAILS_DEFAULT_LOGGER
+      
+      cattr_accessor :logger
+      
       def method_name
         raise Exception.new("method_name not implemented.")
       end
--- lib/workling/routing/static_routing.rb
+++ lib/workling/routing/static_routing.rb
@@ -5,7 +5,7 @@ require 'workling/routing/base'
 #
 module Workling
   module Routing
-    class StaticRouting
+    class StaticRouting < Base
 
       # ./script/workling_client run -- <worker_class> <worker_method> <routing_key>      
       # ./script/workling_client run -- CowWorker moo "#"
@@ -16,39 +16,26 @@ module Workling
         @routing_key = args[2]
         @queue_name = [@worker.class.to_s.tableize, @method_name, @routing_key].join("__")
 
-        puts "** static routing: queue - #{@queue_name}, routing_key - #{@routing_key}, method - #{@method_name}, worker - #{@worker.class}"
-      end
+        # so routing[x] hash access works as expected
+        self.default = @worker
 
-      # TODO - redo this...
-      def [](x)
-        @worker
+        logger.debug "** static routing: queue - #{@queue_name}, routing_key - #{@routing_key}, method - #{@method_name}, worker - #{@worker.class}"
       end
 
       # returns the worker method name, given the routing string. 
-      def method_name(queue=nil)
-        @method_name
-      end
+      def method_name(queue=nil); @method_name; end
 
-      def routing_key_for
-        @routing_key
-      end
+      def routing_key_for; @routing_key; end
 
       # returns the routing string, given a class and method. delegating.
       # TODO - we can check for consistency here with clazz and methods vs. current configuration of this single route
-      def queue_for(clazz=nil, method=nil)
-        @queue_name
-      end
+      def queue_for(clazz=nil, method=nil); @queue_name; end
    
-      # returns all routed
-      def queue_names
-        [@queue_name]
-      end
+      # returns array containing the single configured route
+      def queue_names; [@queue_name]; end
       
-      # dare you to remove this! go on!
       # TODO - not sure what this is...
-      def queue_names_routing_class(clazz)
-        @worker.class
-      end         
+      def queue_names_routing_class(clazz); @worker.class; end         
     end
   end
 end
