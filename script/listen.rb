a0161cf4f1e2e80aaabdc3d2412572e92ddca948
Simon Horne <soulware@gmail.com>
2009-04-03 16:29:53 -0700

    pass in optional 4th arg to routing

--- script/listen.rb
+++ script/listen.rb
@@ -10,7 +10,9 @@ require File.dirname(__FILE__) + '/../lib/workling/routing/class_and_method_rout
 client = Workling::Remote.dispatcher.client
 invoker = Workling::Remote.invoker
 routing = Workling::Remote.routing
-poller = invoker.new(routing.new(ARGV[0], ARGV[1], ARGV[2]), client.class)
+
+# TODO - must be a better way of passing in ARGV here...
+poller = invoker.new(routing.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3]), client.class)
 
 puts '** Rails loaded.'
 puts "** Starting #{invoker}, #{routing} ..."
